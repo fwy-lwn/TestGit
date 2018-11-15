490cdab2e810d36203352993b9b733e7e257a42b
dusto <fwy9601@126.com>
2018-11-15 22:41:58 +0800

    解决冲突

--- src/main/java/com/fan/git/Haay.java
+++ src/main/java/com/fan/git/Haay.java
@@ -2,7 +2,7 @@ package com.fan.git;
 
 public class Haay {
     public static void main(String[] args) {
+        System.out.println("right ...");
         System.out.println("left ...");
-        System.out.println("hot fix ...");
     }
 }
